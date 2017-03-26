33a528e11937687c3e0361cf1da21a8b2b4dcfd9
JackieGauthier <jackiegauthier8@gmail.com>
2017-03-25 20:56:00 -0400

    made recorder a class

--- witia/record.py
+++ witia/record.py
@@ -1,35 +1,38 @@
 import pyaudio
 import wave
 
-FORMAT = pyaudio.paInt16
-CHANNELS = 2
-RATE = 44100
-CHUNK = 1024
-RECORD_SECONDS = 5
-WAVE_OUTPUT_FILENAME = "file.wav"
+class recorder():
 
-audio = pyaudio.PyAudio()
+    def record(self,seconds=10):
+        FORMAT = pyaudio.paInt16
+        CHANNELS = 2
+        RATE = 44100
+        CHUNK = 1024
+        RECORD_SECONDS = seconds
+        WAVE_OUTPUT_FILENAME = "file.wav"
 
-# start Recording
-stream = audio.open(format=FORMAT, channels=CHANNELS,
-                    rate=RATE, input=True,
-                    frames_per_buffer=CHUNK)
-print "recording..."
-frames = []
+        audio = pyaudio.PyAudio()
 
-for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
-    data = stream.read(CHUNK)
-    frames.append(data)
-print "finished recording"
+        # start Recording
+        stream = audio.open(format=FORMAT, channels=CHANNELS,
+                            rate=RATE, input=True,
+                            frames_per_buffer=CHUNK)
+        print ("recording...")
+        frames = []
 
-# stop Recording
-stream.stop_stream()
-stream.close()
-audio.terminate()
+        for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
+            data = stream.read(CHUNK)
+            frames.append(data)
+        print ("finished recording")
 
-waveFile = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
-waveFile.setnchannels(CHANNELS)
-waveFile.setsampwidth(audio.get_sample_size(FORMAT))
-waveFile.setframerate(RATE)
-waveFile.writeframes(b''.join(frames))
-waveFile.close()
+        # stop Recording
+        stream.stop_stream()
+        stream.close()
+        audio.terminate()
+
+        waveFile = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
+        waveFile.setnchannels(CHANNELS)
+        waveFile.setsampwidth(audio.get_sample_size(FORMAT))
+        waveFile.setframerate(RATE)
+        waveFile.writeframes(b''.join(frames))
+        waveFile.close()
