a3ad05404a5a8d15a9c0b213bffe455d7978f5f5
agoestchm <anselme.goetschmann@gmail.com>
2017-03-25 20:17:47 -0400

    Unpack wit answer

--- witia/send_to_wit.py
+++ witia/send_to_wit.py
@@ -1,9 +1,10 @@
 from wit import Wit
+# https://github.com/wit-ai/pywit
 import os
 
 client = Wit(access_token=os.environ.get('WIT_TOKEN'))
 
 resp = None
 with open('file.wav', 'rb') as f:
-    resp = client.speech(f, None, {'Content-Type': 'audio/wav'})
-print('Yay, got Wit.ai response: ' + str(resp))
+    resp = client.speech(f, None, headers={'Content-Type': 'audio/wav', 'Accept': 'application/json'})
+print('Yay, got Wit.ai response: ' + str(resp['_text']))
