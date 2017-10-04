d091a196cf749117077f29ce6069573c85446cb4
agoestchm <anselme.goetschmann@gmail.com>
2017-10-04 14:19:43 +0200

    Before beginning v2

--- main.py
+++ main.py
@@ -4,26 +4,28 @@ from wolfram.wolfram_alpha_api import *
 from latex.preprocess import *
 from latex.print_latex import *
 
-def speech_to_latex(audio_file):
-    # record('audio_input')
-    # "integral of x squared"
-    # "sum from x equal one to ten of x "
-    print('Waiting on Wit.ai ...')
+# def speech_to_latex(audio_file):
+record('audio_input')
+audio_file = 'audio_input'
+# "integral of x squared"
+# "sum from x equal one to ten of x "
+print('Waiting on Wit.ai ...')
 
-    wit_resp = send_audio_to_wit(audio_file)
-    # wit_resp = send_audio_to_wit('audio_input')
+wit_resp = send_audio_to_wit(audio_file)
+# wit_resp = send_audio_to_wit('audio_input')
 
-    print('Wit.ai response: ' + wit_resp)
+print('Wit.ai response: ' + wit_resp)
 
-    if(len(wit_resp) > 0):
-        wa_resp = wolfram_interpret(wit_resp)
+if (len(wit_resp) > 0):
+    # wa_resp = wolfram_interpret(wit_resp)
+    #
+    # print("Wolfram Alpha interpretation: " + str(wa_resp))
+    #
+    # if(wa_resp != None and len(wa_resp) > 0):
+    #     preprocessed = preprocess(wa_resp)
+    preprocessed = preprocess(wit_resp)
 
-        print("Wolfram Alpha interpretation: " + str(wa_resp))
+    print("Preprocessed expression: " + preprocessed)
+    print_latex(preprocessed, 'dest')
 
-        if(wa_resp != None and len(wa_resp) > 0):
-            preprocessed = preprocess(wa_resp)
-
-            print("Preprocessed expression: " + preprocessed)
-            print_latex(preprocessed, 'dest')
-
-            return (wit_resp, wa_resp, preprocessed, "dest-1.png")
+    # return (wit_resp, wa_resp, preprocessed, "dest-1.png")
