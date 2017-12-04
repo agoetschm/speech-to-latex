a0be4b9e06a0ae564505cabdcec6cba9e38e4371
agoestchm <anselme.goetschmann@gmail.com>
2017-12-04 23:09:07 +0100

    Add postinstall in package.json

--- server.js
+++ server.js
@@ -4,22 +4,11 @@
 const express = require('express');
 const path = require('path');
 const http = require('http');
-const bodyParser = require('body-parser');
-
-// Get our API routes
-const api = require('./server/routes');
 
 const app = express();
 
-// Parsers for POST data
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-
 // Point static path to dist
 app.use(express.static(path.join(__dirname, 'dist')));
-
-// Set our api routes
-app.use('/api', api);
 
 app.use(express.static(path.join(__dirname, 'public'))); // for the katex fonts
 app.use(express.static(path.join(__dirname, 'node_modules/opus-recorder/dist')));
